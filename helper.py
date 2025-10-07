043fcbef0430ad8ca557fc5f54d5615cba32aa1b
Stefan Teske <steske@live.nl>
2025-10-07 22:00:28 +0200

    Nou dit was me een oefening wel, les 10

--- helper.py
+++ helper.py
@@ -5,3 +5,20 @@ def decoreer(tekst=""):
     print(f"* {tekst} *")
     print(lengte * "*")
     print()
+
+def fooi_pp(bedrag, personen):
+    try:
+        bedrag_pp = round(bedrag / personen,2)
+    except:
+        bedrag_pp = "??"
+    return(f"De gezamenlijke fooienpot komt neer op {bedrag_pp} euro per persoon.")
+
+def onderstreept(tekst=""):
+    uit = []
+    uit.append(tekst)
+    uit.append(len(tekst) * "=")
+    return uit
+
+def som(lijst):
+    telop = sum(lijst.values())
+    return telop
---
+++ presentatie.py
@@ -0,0 +1,5 @@
+def presenteer(mijn_dict, totaal):
+    for k, v in mijn_dict.items():
+        print(k, ":", v, "euro")
+    print("==========================")
+    print("totaal : ", totaal, "euro")
